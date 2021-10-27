db1bc05132847237c334a23bcf119456d2b7e3a7
Anas Limouri <tsudete@gmail.com>
2021-10-27 11:13:33 +0100

    feat: supply country to be shown in case of +1 countries

--- src/demo.jsx
+++ src/demo.jsx
@@ -1,13 +1,13 @@
-import React from 'react';
-import { render } from 'react-dom';
-import MaterialUiPhoneNumber from './index';
-
+import React from "react";
+import { render } from "react-dom";
+import MaterialUiPhoneNumber from "./index";
+import { countries_fr } from "./countries_fr.js";
 class ChangeDemo extends React.Component {
   constructor(props) {
     super(props);
 
     this.state = {
-      number: '',
+      number: "",
     };
   }
 
@@ -16,15 +16,11 @@ class ChangeDemo extends React.Component {
 
     return (
       <div>
-        Current number:
-        {' '}
-        {number}
-
+        Current number: {number}
         <br />
-
         <MaterialUiPhoneNumber
           defaultCountry="it"
-          preferredCountries={['it', 'se']}
+          preferredCountries={["it", "se"]}
           onChange={(e) => {
             this.setState({ number: e });
           }}
@@ -35,81 +31,72 @@ class ChangeDemo extends React.Component {
 }
 
 export default render(
-  <div style={{ fontFamily: "'Roboto', sans-serif", fontSize: '15px' }}>
-    <div style={{ display: 'inline-block', verticalAlign: 'top' }}>
+  <div style={{ fontFamily: "'Roboto', sans-serif", fontSize: "15px" }}>
+    <div style={{ display: "inline-block", verticalAlign: "top" }}>
       <p>v1.1.0</p>
       <p>Exclude countries (usa, canada)</p>
       <MaterialUiPhoneNumber
+        onChange={(e) => console.log(e)}
         defaultCountry="no"
-        excludeCountries={['us', 'ca']}
+        excludeCountries={["us", "ca"]}
+        localization={countries_fr}
       />
       <p>Only countries</p>
-      <MaterialUiPhoneNumber
-        defaultCountry="gb"
-        onlyCountries={['gb', 'es']}
-      />
+      <MaterialUiPhoneNumber defaultCountry="gb" onlyCountries={["gb", "es"]} />
       <p>Preferred countries</p>
       <MaterialUiPhoneNumber
         defaultCountry="it"
-        preferredCountries={['it', 'se']}
+        preferredCountries={["it", "se"]}
       />
       <p>onChange</p>
       <ChangeDemo />
       <p>Native</p>
       <MaterialUiPhoneNumber
         defaultCountry="it"
-        preferredCountries={['it', 'se']}
+        preferredCountries={["it", "se"]}
         native
       />
     </div>
 
-    <div style={{ display: 'inline-block', marginLeft: '40px' }}>
+    <div style={{ display: "inline-block", marginLeft: "40px" }}>
       <p>v2</p>
       <p>Auto country detect by value</p>
       <MaterialUiPhoneNumber
-        value="+3802343252"
+        disableAreaCodes
+        defaultCountry="pr"
+        value="+1882343252"
       />
       <p>Disabled area codes with disableAreaCodes</p>
-      <MaterialUiPhoneNumber
-        defaultCountry="us"
-        disableAreaCodes
-      />
+      <MaterialUiPhoneNumber defaultCountry="us" disableAreaCodes />
       <p>Disabled flag by default</p>
       <p>Customizable placeholder</p>
     </div>
 
-    <div style={{
-      display: 'inline-block', marginLeft: '40px', verticalAlign: 'top', marginTop: '35px',
-    }}
+    <div
+      style={{
+        display: "inline-block",
+        marginLeft: "40px",
+        verticalAlign: "top",
+        marginTop: "35px",
+      }}
     >
-      <p>
-        Custom region selected:
-        {' '}
-        {'{\'europe\'}'}
-      </p>
-      <MaterialUiPhoneNumber
-        defaultCountry="it"
-        regions="europe"
-      />
-      <p>
-        Custom regions selected:
-        {' '}
-        {'{[\'north-america\', \'carribean\']}'}
-      </p>
+      <p>Custom region selected: {"{'europe'}"}</p>
+      <MaterialUiPhoneNumber defaultCountry="it" regions="europe" />
+      <p>Custom regions selected: {"{['north-america', 'carribean']}"}</p>
       <MaterialUiPhoneNumber
         defaultCountry="ca"
-        regions={['north-america', 'carribean']}
+        regions={["north-america", "carribean"]}
       />
       <p>Disabled dropdown</p>
       <MaterialUiPhoneNumber
-        onlyCountries={['us']}
+        onlyCountries={["us"]}
         defaultCountry="us"
         disableDropdown
         placeholder="(702) 123-4567"
       />
       <p>Disabled dropdown and country code</p>
       <MaterialUiPhoneNumber
-        onlyCountries={['us']}
+        onlyCountries={["us"]}
         defaultCountry="us"
         disableAreaCodes
         disableCountryCode
@@ -118,5 +105,5 @@ export default render(
       />
     </div>
   </div>,
-  document.getElementById('root'),
+  document.getElementById("root")
 );
